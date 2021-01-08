36057ebfcb6ef514be0aefdcda77e4554e29e595
Dwayne Weller <mr_dwayne_w@yahoo.com>
2021-01-08 07:40:10 +0000

    Automatically backed up by Learn

--- js/createStore.js
+++ js/createStore.js
@@ -1,5 +1,10 @@
 function createStore() {
   let state;
+  
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  };
 }
 
 function reducer(state = { count: 0 }, action) {
@@ -10,11 +15,6 @@ function reducer(state = { count: 0 }, action) {
     default:
       return state;
   }
-};
-
-function dispatch(action){
-  state = reducer(state, action);
-  render();
 };
 
 function render() {
